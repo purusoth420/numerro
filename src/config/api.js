c3acaeb282b06f5615c05d1dfa99996f869fee43
Purusoth_s <purusothaman@angleritech.com>
2022-07-20 18:10:33 +0530

    'images api integrated'

---
+++ src/config/api.js
@@ -0,0 +1,8 @@
+
+export const homepage = {
+    pixabay : 'https://pixabay.com/api/?key=',
+    pixabaykey : '28701053-77bb3ba75e209ee18ae00ab97',
+    pexels : 'https://api.pexels.com/v1/search/?page=2&per_page=20&query=',
+    // pexels : 'https://api.pexels.com/v1/curated',search/?page=2&per_page=1&query=nature
+    pexelskey : '563492ad6f9170000100000116be1b142b274dfe8b03e28dc493ded0'
+}
--- src/pages/Home/home.js
+++ src/pages/Home/home.js
@@ -3,11 +3,17 @@ import './home.scss';
 import { SketchPicker } from 'react-color';
 import axios from 'axios';
 // import {FontPicker} from 'font-picker';
+import { homepage } from '../../config/api'
+import pixabay from '../../assets/images/pixabay.png'
+import pexel from '../../assets/images/pexels.png'
 
 export const Home = () => {
   const key = "28701053-77bb3ba75e209ee18ae00ab97"
   const [images, setImages] = useState([])
+  const [pexelimg, setpexelimg] = useState([])
+  const [pexels, setpexels] = useState('')
   const [search, setsearch] = useState('')
+  const [show, setShow] = useState(0)
 
   useEffect(() => {
     // handleImages();
@@ -20,10 +26,29 @@ export const Home = () => {
     var value = e.target.value
     setsearch(value);
     try {
-      await axios.get(`https://pixabay.com/api/?key=` + key +"&q=" + value).then(
+      await axios.get(homepage.pixabay + homepage.pixabaykey + "&q=" + value).then(
         (res) => {
           if (res.status == 200) {
             setImages(res.data)
+          }
+        })
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async function handlePexels(e) {
+    var value = e.target.value
+    setpexels(value);
+    try {
+      await axios.get(homepage.pexels + value, {
+        headers: {
+          Authorization: homepage.pexelskey
+        }
+      }).then(
+        (res) => {
+          if (res.status == 200) {
+            setpexelimg(res.data)
           }
         })
     } catch (error) {
@@ -54,21 +79,64 @@ export const Home = () => {
         <div>
           <SketchPicker className='colorpicker' />
         </div>
-        <div>
-          <input value={search} onChange={(e)=> handleImages(e)}></input>
-        </div>
-        <div style={{ display: 'flex', justifyContent: 'center' }}>
-          <div className='images'>
-            {
-              images == "" ? <p>Loading...</p> :
-                images.hits.map((i) => {
-                  return <div key={i.id} className="pixabay" >
-                    <img src={i.previewURL} width='75px' height='75px' />
-                  </div>
-                })
-            }
-          </div>
-        </div>
+        {
+          show == 0 ?
+
+            < >
+              <div className='search'>
+                <div>
+                  {/* <input value={search} className='form-control input-sm' onChange={(e) => handleImages(e)}></input> */}
+                  <select class="form-control input-sm" value={show} onChange={(e) => setShow(e.target.value)}>
+                    <option value={0}>Pixabay</option>
+                    <option value={1}>Pexels</option>
+                  </select>
+                </div>
+                <div>
+                  <input value={search} className='form-control input-sm' onChange={(e) => handleImages(e)}></input>
+                </div>
+              </div>
+              <div style={{ display: 'flex', justifyContent: 'center' }}>
+                <div className='images'>
+                  {
+                    images == "" ? <img src={pixabay} ></img> :
+                      images.hits.map((i) => {
+                        return <div key={i.id} className="pixabay" >
+                          <img src={i.previewURL} />
+                        </div>
+                      })
+                  }
+                </div>
+              </div>
+            </ >
+            :
+
+            < >
+              <div className='search'>
+                <div>
+                  {/* <input value={search} className='form-control input-sm' onChange={(e) => handleImages(e)}></input> */}
+                  <select class="form-control input-sm" value={show} onChange={(e) => setShow(e.target.value)}>
+                    <option value={0}>Pixabay</option>
+                    <option value={1}>Pexels</option>
+                  </select>
+                </div>
+                <div>
+                  <input value={pexels} className='form-control input-sm' onChange={(e) => handlePexels(e)}></input>
+                </div>
+              </div>
+              <div style={{ display: 'flex', justifyContent: 'center' }}>
+                <div className='images'>
+                  {
+                    pexelimg == "" ? <img src={pexel} ></img> :
+                      pexelimg.photos.map((i) => {
+                        return <div key={i.id} className="pixabay" >
+                          <img src={i.src.tiny} />
+                        </div>
+                      })
+                  }
+                </div>
+              </div>
+            </ >
+        }
       </div>
     </div>
   )
